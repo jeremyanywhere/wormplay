70e57cd2163c23e977343c286f3de7d7a780ad14
Jeremy <jeremycustomer@gmail.com>
2022-01-19 18:45:39 -0500

    Added Recursive Tree Datastructure

--- makewordsquares.py
+++ makewordsquares.py
@@ -2,9 +2,33 @@
 #Thoughts, comments, improvements welcome.
 
 import random 
+import time
 
 f1 = open ('5letterwords.txt')
 rw = f1.read().splitlines() 
+word_tree = {}
+
+def populate_tree(word, branch):
+	if len(word) == 0:
+		return
+	if word[0] in branch:
+		n = branch[word[0]]
+		populate_tree(word[1:len(word)],n)
+	else:  # word not in tree yet.
+		branch[word[0]]= {}
+		populate_tree(word[1:len(word)],branch[word[0]])
+
+def get_matches(tree, pattern, match_list, constructed_word):
+	if len(pattern) == 0:
+		match_list.append(constructed_word)
+		constructed_word=""
+	else: 
+		if pattern[0] in tree:
+			constructed_word+=pattern[0]
+			get_matches(tree[pattern[0]],pattern[1:len(pattern)], match_list, constructed_word)
+		if pattern[0] == '?':
+			for letter in tree:
+				get_matches(tree[letter],pattern[1:len(pattern)],match_list, constructed_word+letter)
 
 def dump_string_as_grid(s):
 	for i in range(5):
@@ -13,8 +37,12 @@ def dump_string_as_grid(s):
 
 def match(word, letter, index):
 	return word[index] == letter
-
 def get_matching_list(pattern):
+	match_list = []
+	get_matches(word_tree, pattern, match_list, "")
+	return match_list
+
+def get_matching_list_non_recursive(pattern):
 	r = range(len(pattern))
 	result_list = []
 	for w in rw:
@@ -27,26 +55,26 @@ def get_matching_list(pattern):
 			result_list.append(w)
 	return result_list
 
-def try_next_word(s, n):
+def try_next_word(grid, n):
 	if (n>9):
-		dump_string_as_grid(s)
+		dump_string_as_grid(grid)
 		return
 	match_list = []
 	if (n % 2) == 0: # even, 
 		m = ""
 		for x in range(5):
-			m = m + s[int(n/2)*5+x]
+			m = m + grid[int(n/2)*5+x]
 			match_list = get_matching_list(m)
 	if (n % 2) != 0: #pull out verticals
 		m = ""
 		for x in range(5):
-			m = m + s[int((n-1)/2)+x*5]
+			m = m + grid[int((n-1)/2)+x*5]
 			match_list = get_matching_list(m)
 	if len(match_list) < 1:  # recursion ends.
 		return
 
 	for w in match_list:
-		lw = list(s)
+		lw = list(grid)
 		if (n % 2) == 0: #even
 			for x in range(5):
 				lw[int(n/2)*5+x] = w[x]
@@ -61,6 +89,20 @@ def try_next_word(s, n):
 
 
 # random.shuffle(rw)
+for w in rw:
+	populate_tree(w, word_tree)
+
+ts = time.time()
+print(f"added word tree, length of top node:{len(word_tree)}")
+match_list = []
+p = "??p??"
+ts = time.time()
+match_list = get_matching_list(p)
+print(f"matching {p}, with tree lookup, time taken: {time.time()-ts}ms, match list: {len(match_list)}")
+
+ts = time.time()
+match_list = get_matching_list_non_recursive(p)
+print(f"matching {p}, with linear list look up, time taken: {time.time()-ts}ms,match list: {len(match_list)}")
 
 for seed in rw:
 	print (f"seeding with - {seed}")
